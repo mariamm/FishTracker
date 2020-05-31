abfdca0a01a5563e70415f131f6052d322d159e7
Mariam Mahmoud <mariem.mahmoud@gmail.com>
2020-05-31 18:49:48 +0200

    Add missing license

--- sources/libraries/imageprocessing/Common.h
+++ sources/libraries/imageprocessing/Common.h
@@ -12,23 +12,28 @@
 
 
 /*
-Endianness
----
-Although not totally clear from the docs, some of QImage's formats we use here are
-endian-dependent. For example:
-Little Endian
-QImage::Format_ARGB32 the bytes are ordered:    B G R A
-QImage::Format_RGB32 the bytes are ordered:     B G R (255)
-QImage::Format_RGB888 the bytes are ordered:    R G B
-Big Endian
-QImage::Format_ARGB32 the bytes are ordered:    A R G B
-QImage::Format_RGB32 the bytes are ordered:     (255) R G B
-QImage::Format_RGB888 the bytes are ordered:    R G B
-Notice that Format_RGB888 is the same regardless of endianness. Since OpenCV
-expects (B G R) we need to swap the channels for this format.
-This is why some conversions here swap red and blue and others do not.
-This code assumes little endian. It would be possible to add conversions for
-big endian machines though. If you are using such a machine, please feel free
+
+   Functions to convert between OpenCV's cv::Mat and Qt's QImage and QPixmap.
+   Andy Maloney <asmaloney@gmail.com>
+   https://asmaloney.com/2013/11/code/converting-between-cvmat-and-qimage-or-qpixmap
+
+    Endianness
+    ---
+    Although not totally clear from the docs, some of QImage's formats we use here are
+    endian-dependent. For example:
+    Little Endian
+    QImage::Format_ARGB32 the bytes are ordered:    B G R A
+    QImage::Format_RGB32 the bytes are ordered:     B G R (255)
+    QImage::Format_RGB888 the bytes are ordered:    R G B
+    Big Endian
+    QImage::Format_ARGB32 the bytes are ordered:    A R G B
+    QImage::Format_RGB32 the bytes are ordered:     (255) R G B
+    QImage::Format_RGB888 the bytes are ordered:    R G B
+    Notice that Format_RGB888 is the same regardless of endianness. Since OpenCV
+    expects (B G R) we need to swap the channels for this format.
+    This is why some conversions here swap red and blue and others do not.
+    This code assumes little endian. It would be possible to add conversions for
+    big endian machines though. If you are using such a machine, please feel free
 to submit a pull request on the GitHub page.
 */
 #if Q_BYTE_ORDER == Q_BIG_ENDIAN
