89a699ecbcc85d832ff01b1feebce418b5773f10
Mariam Mahmoud <mariam.mahmoud@uni-konstanz.de>
2021-04-28 09:47:44 +0200

    Remove useless things

--- sources/application/Common.h
+++ sources/application/Common.h
@@ -3,15 +3,11 @@
 #include <vector>
 #include <QImage>
 #include <QPixmap>
-#include <QMap>
 #include <QDebug>
 
 #include <opencv2/opencv.hpp>
 
-
-
 /*
-
    Functions to convert between OpenCV's cv::Mat and Qt's QImage and QPixmap.
    Andy Maloney <asmaloney@gmail.com>
    https://asmaloney.com/2013/11/code/converting-between-cvmat-and-qimage-or-qpixmap
@@ -41,12 +37,6 @@ to submit a pull request on the GitHub page.
 
 namespace Common 
 {
-
-    /**
-        Functions to convert between OpenCV's cv::Mat and Qt's QImage and QPixmap.
-        Andy Maloney <asmaloney@gmail.com>
-        https://asmaloney.com/2013/11/code/converting-between-cvmat-and-qimage-or-qpixmap
-    **/
     QImage  cvMatToQImage(const cv::Mat &inMat);
 
     QPixmap cvMatToQPixmap(const cv::Mat &inMat);
@@ -56,58 +46,4 @@ namespace Common
 
     // cloneData false if inPixmap exists for the lifetime of the resulting cv::Mat
     cv::Mat qPixmapToCvMat(const QPixmap &inPixmap, bool cloneData = true);
-
-    static QMap<QImage::Format, QString>   sImageFormatEnumMap = {
-                                                                    { QImage::Format::Format_Invalid, "Format_Invalid" },
-                                                                    { QImage::Format::Format_Mono, "Format_Mono" },
-                                                                    { QImage::Format::Format_MonoLSB, "Format_MonoLSB" },
-                                                                    { QImage::Format::Format_Indexed8, "Format_Indexed8" },
-                                                                    { QImage::Format::Format_RGB32, "Format_RGB32" },
-                                                                    { QImage::Format::Format_ARGB32, "Format_ARGB32" },
-                                                                    { QImage::Format::Format_ARGB32_Premultiplied, "Format_ARGB32_Premultiplied" },
-                                                                    { QImage::Format::Format_RGB16, "Format_RGB16" },
-                                                                    { QImage::Format::Format_ARGB8565_Premultiplied, "Format_ARGB8565_Premultiplied" },
-                                                                    { QImage::Format::Format_RGB666, "Format_RGB666" },
-                                                                    { QImage::Format::Format_ARGB6666_Premultiplied, "Format_ARGB6666_Premultiplied" },
-                                                                    { QImage::Format::Format_RGB555, "Format_RGB555" },
-                                                                    { QImage::Format::Format_ARGB8555_Premultiplied, "Format_ARGB8555_Premultiplied" },
-                                                                    { QImage::Format::Format_RGB888, "Format_RGB888" },
-                                                                    { QImage::Format::Format_RGB444, "Format_RGB444" },
-                                                                    { QImage::Format::Format_ARGB4444_Premultiplied, "Format_ARGB4444_Premultiplied" },
-                                                                    { QImage::Format::Format_RGBX8888, "Format_RGBX8888" },
-                                                                    { QImage::Format::Format_RGBA8888, "Format_RGBA8888" },
-                                                                    { QImage::Format::Format_RGBA8888_Premultiplied, "Format_RGBA8888_Premultiplied" },
-                                                                    { QImage::Format::Format_BGR30, "Format_BGR30" },
-                                                                    { QImage::Format::Format_A2BGR30_Premultiplied, "Format_A2BGR30_Premultiplied" },
-                                                                    { QImage::Format::Format_RGB30, "Format_RGB30" },
-                                                                    { QImage::Format::Format_A2RGB30_Premultiplied, "Format_A2RGB30_Premultiplied" },
-                                                                    { QImage::Format::Format_Alpha8, "Format_Alpha8" },
-                                                                    { QImage::Format::Format_Grayscale8, "Format_Grayscale8" }
-                                                                };
-
-    static QString sQImageFormatToStr(QImage::Format inFormat)
-    {
-        return sImageFormatEnumMap[inFormat];
-    }
-
-    static QString sCVTypeToStr(int inType)
-    {
-        QString str("CV_");
-
-        switch (CV_MAT_DEPTH(inType))
-        {
-        case CV_8U:    str += "8U"; break;
-        case CV_8S:    str += "8S"; break;
-        case CV_16U:   str += "16U"; break;
-        case CV_16S:   str += "16S"; break;
-        case CV_32S:   str += "32S"; break;
-        case CV_32F:   str += "32F"; break;
-        case CV_64F:   str += "64F"; break;
-        default:       str += "User"; break;
-        }
-
-        str += QStringLiteral("C%1").arg(QString::number(CV_MAT_CN(inType)));
-
-        return str;
-    }
 };
